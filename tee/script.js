5d8b3001a27f9ed284a295a517075d6d63f86e5b
3bbydd <51096409+3bbydd@users.noreply.github.com>
2019-06-12 02:45:37 +0300

    Add files via upload

---
+++ tee/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	 'use strict';	
+  $('header .arrow i').on('click',function() {
+    $('html , body').animate({
+      scrollTop: $('.features').offset().top
+    });
+      
+      
+  });
+	$('.show-more').on('click',function() {
+      $(this).fadeOut(200,function() {
+        $('.our-work .hidden').fadeIn(1000);
+      });
+    
+    
+  })
+});
+
