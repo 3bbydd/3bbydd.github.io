e778443f3ff997e8f8c2b62594e7894eb298432c
3bbydd <51096409+3bbydd@users.noreply.github.com>
2019-06-14 00:29:07 +0300

    Add files via upload

---
+++ tee/script.js
@@ -0,0 +1,97 @@
+$(function(){
+  var leftArrow = $('.testi .fa-chevron-left'),
+  rightArrow = $('.testi .fa-chevron-right');
+
+
+	 'use strict';	
+  $('header .arrow i').on('click',function() {
+    $('html , body').animate({
+      scrollTop: $('.features').offset().top
+    },1000);
+      
+      
+  });
+  
+   $('.buttons .hire').on('click',function() {
+    $('html , body').animate({
+      scrollTop: $('.our-team').offset().top
+    },1000);
+      
+      
+  });
+  
+   $('.buttons .works').on('click',function() {
+    $('html , body').animate({
+      scrollTop: $('.our-work').offset().top
+    },1000);
+      
+      
+  });
+  
+  
+	$('.show-more').on('click',function() {
+    
+    $('.our-work .hidden').fadeIn(1000);
+    $(this).fadeOut();
+  });
+
+  
+
+  function cheakClients() {
+   
+    $('.client:first').hasClass('active') ? rightArrow.fadeOut() :rightArrow.fadeIn(); 
+    $('.client:last').hasClass('active')  ? leftArrow.fadeOut()  :leftArrow.fadeIn();
+  }
+  cheakClients();
+
+  $('.testi i').on('click' , function(){
+ if ($(this).hasClass('fa-chevron-left')){
+   $('.testi .active').fadeOut(100,function(){
+     $(this).removeClass('active').next('.client').addClass('active').fadeIn();
+     cheakClients();
+   });
+ } else {
+   $('.testi .active').fadeOut(100,function(){
+      $(this).removeClass('active').prev('.client').addClass('active').fadeIn(1000);
+      cheakClients();
+    });
+ }
+  });
+  
+  
+  // القائمة الجانبية
+  
+  $('.fa-bars').on('click',function() {
+  //  $('.menu').css('top','0')
+  $('.menu').slideToggle()
+  })
+  
+  
+  
+  
+  $('.menu').on('click','li',function() {
+ 
+    
+   
+$('html , body').animate({
+  
+      
+      scrollTop:  $($(this).data("to")).offset().top
+    },1000);
+    
+    $('.menu').slideUp();
+    
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+});
+
