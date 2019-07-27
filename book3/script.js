68c1c0612671ba50be68648608df01dd6e2ea91c
3bbydd <51096409+3bbydd@users.noreply.github.com>
2019-07-28 02:18:28 +0300

    Add files via upload

--- book3/script.js
+++ book3/script.js
@@ -1,1 +1,18 @@
 
+window.addEventListener("touchstart", tapHandler);
+
+var tapedTwice = false;
+
+function tapHandler(event) {
+    if(!tapedTwice) {
+        tapedTwice = true;
+        setTimeout( function() { tapedTwice = false; }, 300 );
+        return false;
+    }
+    event.preventDefault();
+    //action on double tap goes below
+    
+  window.scrollBy(0, window.innerHeight / 2);
+  
+ }
+  
